be0afa59a0d65a492d446a61d257c0c70c9338c5
Kseniya <kcu_pri@mail.ru>
2022-12-09 22:43:21 +0300

    z2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine($"{num1} больше {num2}");
+}
+else if(num1 < num2)
+{
+    Console.WriteLine($"{num2} больше {num1}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
